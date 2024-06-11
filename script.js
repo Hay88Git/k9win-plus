68d36f6e7751a686965798a4eabeee74c9c0533f
Hay88Git <myvercel2024@gmail.com>
2024-06-11 08:30:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function LinkTo() {
+    location.href = "https://www.hay88.one/?inviteCode=0591862";
+}
+
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 80) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-15%";
+    }
+}
